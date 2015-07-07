dc92339f3179f2cce61a63d6d163bbb7165af40e
ScariestHeat <hipllfire2@outlook.com>
2015-07-06 23:17:11 -0700

    Scripts

---
+++ Click Scripts/Click.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Click : MonoBehaviour {
+
+	public UnityEngine.UI.Text gpc;
+	public UnityEngine.UI.Text GoldDisplay;
+	public float gold = 0.00f;
+	public int goldperclick = 1;
+
+	void Update(){
+		GoldDisplay.text = "Gold: " + CurrencyConverter.Instance.GetCurrencyIntoString(gold, false, false);
+		gpc.text = CurrencyConverter.Instance.GetCurrencyIntoString(goldperclick, false, true) + " gold/click";
+	}
+
+	public void Clicked(){
+		gold += goldperclick;
+	}
+
+}
---
+++ Click Scripts/ItemManager.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ItemManager : MonoBehaviour {
+
+	public UnityEngine.UI.Text iteminfo;
+	public Click click;
+	public float cost;
+	public int tickValue;
+	public int count;
+	public string itemName;
+	public Color standard;
+	public Color affordable;
+	private float baseCost;
+
+	void Start(){
+		baseCost = cost;
+	}
+
+	void Update(){
+		iteminfo.text = itemName + "\nCost: " + CurrencyConverter.Instance.GetCurrencyIntoString(cost, false, false) + "\nGold: " + tickValue + "/s";
+		if(click.gold >= cost) {
+			GetComponent<Image> ().color = affordable;
+		} else {
+			GetComponent<Image> ().color = standard;
+		}
+	}
+
+	public void PurchasedItem(){
+		if (click.gold >= cost) {
+			click.gold -= cost;
+			count += 1;
+			cost = Mathf.Round (baseCost * Mathf.Pow (1.15f, count));
+		}
+
+
+	}
+
+}
